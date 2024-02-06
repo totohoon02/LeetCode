54dbd7589fb682665bc8b6896215e446f01cadb2
SangHoon Kang <53715807+totohoon02@users.noreply.github.com>
2024-02-06 10:09:00 +0900

    Time: 37 ms (52.67%), Space: 16.5 MB (76.07%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,19 @@
+#Stack from Queue
+from collections import * 
+
+class MyStack:
+
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, x: int) -> None:
+       self.stack.append(x) 
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
