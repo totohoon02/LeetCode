b890ee7e64b41881e5e56c0d66c1dca05a4fa941
SangHoon Kang <53715807+totohoon02@users.noreply.github.com>
2024-02-06 10:16:50 +0900

    Time: 37 ms (45.19%), Space: 16.7 MB (30.70%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,19 @@
+#Quere from Stack
+class MyQueue:
+
+    def __init__(self):
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        data = self.queue[0]
+        self.queue = self.queue[1:]
+        return data
+
+    def peek(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0
