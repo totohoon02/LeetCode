a804cf27de91d3a4d3dc1727765169f65740f0fe
SangHoon Kang <53715807+totohoon02@users.noreply.github.com>
2024-02-27 11:50:38 +0900

    Time: 41 ms (49.79%), Space: 16.4 MB (90.04%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+
+class Solution:
+    dp = defaultdict(int)
+
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+
+        if self.dp[n]:
+            return self.dp[n]
+
+        self.dp[n] = self.fib(n-1) + self.fib(n-2)
+        return self.dp[n]
