35404a384ea597d3da7c411753484b5364ad724f
SangHoon Kang <53715807+totohoon02@users.noreply.github.com>
2024-05-09 15:30:20 +0900

    Time: 36 ms (76.47%), Space: 16.7 MB (29.15%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,22 +1,10 @@
 import collections
+import re
 
 class Solution:
-    def __init__(self):
-        pass
-
-    def readline(self, as_int=True):
-        import sys
-        input = sys.stdin.readline
-        line = input().split(" ")
-        if len(line) == 1:
-            return int(line[0])
-        if as_int:
-            return list(map(int, line))
-        else:
-            return line
-
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        import re
+        banned = set(banned)
+        
         words = [word for word in re.sub("[\W]", " ", paragraph).lower().split() if word not in banned]
         count = collections.defaultdict(int)
         for word in words:
